f25e9db52a26dc323ec8b7ff75fd07a1c6152e34
LENTO-2 <laminendiaye052@gmail.com>
2026-04-17 16:13:59 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,71 @@
+import streamlit as st
+
+#configuratuion de la page
+st.set_page_config("cv Mouhamed lamine Ndiaye, page_icon, layout=centered")
+
+# titre
+
+st.title("Mouhamed Lamine Ndiaye")
+
+st.markdown("---")
+
+# Informations personnelles
+
+st.write("Mouhamed Lamine Ndiaye")
+
+st.write("Etudiant en Geomatique")
+
+st.write("Email: laminendiaye052@gmail.com")
+
+st.write("Tel: +221 761374394")
+
+st.write("Adresse: Dakar-St-louis")
+
+st.markdown("---")
+
+#profils
+
+st.header("profils")
+
+st.write("Etudiant en premiere annee de BTS en Geomatique et en Sociologie","Passionne par les systemes d'informatique  geographiques(SIG)","la Cartographie, Analyse spatiale, Dessin architectural, Topographie, Python")
+
+#Informations
+
+st.header("Formation")
+
+st.write("**Brevet Technique et Superieur (BTS)**")
+st.write("Centre d'Entrepreneuriat et de Developpement Technique")
+
+st.write("** Licence en Sociologie**")
+
+st.write("**Baccalaureat**")
+
+st.write("UCAD (universite Cheikh Anta Diop)")
+
+#Competences
+
+st.header("Competences")
+
+st.markdown("""
+
+-Maitrise Outils Informatiques
+
+-Cartographie
+
+-Topographie
+
+-Analyse Spatiales
+
+-python
+
+-Dessin Architectural
+
+-Collecte de donnees
+
+-Base de donnees spatiales et relationnelles
+-Pilotage de Drone
+
+""")
+    
+    
+
