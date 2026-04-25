d21cd8bfcd45d9614752a38c6474be850dbdf570
LENTO-2 <laminendiaye052@gmail.com>
2026-04-25 16:44:21 +0000

    Update cv.py

--- cv.py
+++ cv.py
@@ -12,7 +12,7 @@ st.title("Mouhamed Lamine Ndiaye")
 
 st.write("Mouhamed Lamine Ndiaye")
 
-st.write("Technicien Superieureur Géomatique")
+st.write("Tèchnicien Superieureur Géomatique")
 
 st.write("Email: laminendiaye052@gmail.com")
 
@@ -20,7 +20,7 @@ st.write("Adresse: Dakar")
 
 
 
-st.write("Technicien Supeieur en Géomatique ","spécialisé dans les Systémes d'Informations  Géographiques(SIG)","la Cartographie, Analyse spatiale, Dessin architectural, Topographie, Python")
+st.write("Téchnicien Supeieur en Géomatique ","spécialisé dans les Systémes d'Informations  Géographiques(SIG)","la Cartographie, Analyse spatiale, Dessin architectural, Topographie, Python")
 st.write("Licence en Sociologie")
 #Informations
 
@@ -29,8 +29,8 @@ st.header("Parcours")
 st.write("**Licence en Sociologie**")
 st.write("Universite Numerique Cheikh Hamidou Kane (UNCHK)")
          
-st.write("**Brevet Technique et Superieur (BTS)**")
-st.write("Centre d'Entrepreneuriat et de Developpement Technique")
+st.write("**Brevet Téchnique et Superieur (BTS)**")
+st.write("Centre d'Entrepreneuriat et de Developpement Téchnique")
 
 st.write("**Baccalaureat**")
 st.write("UCAD (universite Cheikh Anta Diop)")
